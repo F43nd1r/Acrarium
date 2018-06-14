3f1c2de2a4d57d1c5383bdcc5a95508a5d858212
f43nd1r <lukas.morawietz@gmail.com>
2018-06-14 22:09:43 +0200

    add compression support. Closes #5

--- src/main/java/com/faendir/acra/rest/RestConfiguration.java
+++ src/main/java/com/faendir/acra/rest/RestConfiguration.java
@@ -13,14 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.faendir.acra.rest;
 
 import com.faendir.acra.rest.multipart.Rfc1341MultipartResolver;
+import com.github.ziplet.filter.compression.CompressingFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.lang.NonNull;
 import org.springframework.web.multipart.MultipartResolver;
+
+import javax.servlet.Filter;
 
 /**
  * @author lukas
@@ -32,5 +36,11 @@ public class RestConfiguration {
     @Bean
     public static MultipartResolver multiPartResolver() {
         return new Rfc1341MultipartResolver();
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public Filter gzipFilter() {
+        return new CompressingFilter();
     }
 }
