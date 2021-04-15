cc77ac21fa47a20795e584dc73dd589faf2a8245
f43nd1r <account+github@faendir.com>
2021-04-15 16:55:08 +0200

    use git for faster release script

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Type version to release [Current version: $(./mvnw org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep '^[0-9]')]"
+echo "Type version to release [Current version: $(git describe --abbrev=0 | sed -e "s/^v//")]"
 read -r version
 git tag -a "v$version" -m "Create version $version"
 git push --tags
