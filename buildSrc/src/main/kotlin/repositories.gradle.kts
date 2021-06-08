2f4938ab8c2e42d54b247708f160abb38e79aedf
f43nd1r <account+github@faendir.com>
2021-06-08 18:15:36 +0200

    simplify dependencies

--- buildSrc/src/main/kotlin/repositories.gradle.kts
+++ buildSrc/src/main/kotlin/repositories.gradle.kts
@@ -3,5 +3,6 @@ repositories {
     google()
     maven { setUrl("https://jitpack.io") }
     maven { setUrl("https://maven.vaadin.com/vaadin-addons") }
+    maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
     mavenLocal()
 }
