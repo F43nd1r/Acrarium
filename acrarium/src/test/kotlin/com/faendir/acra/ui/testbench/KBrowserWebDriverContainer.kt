47f0f41bf4f6f8a9c42b1a37542ecd953f977355
f43nd1r <account+github@faendir.com>
2022-07-02 19:29:15 +0200

    remove UI tests due to end of testbench license

--- acrarium/src/test/kotlin/com/faendir/acra/ui/testbench/KBrowserWebDriverContainer.kt
+++
@@ -1,5 +0,0 @@
-package com.faendir.acra.ui.testbench
-
-import org.testcontainers.containers.BrowserWebDriverContainer
-
-class KBrowserWebDriverContainer : BrowserWebDriverContainer<KBrowserWebDriverContainer>()
