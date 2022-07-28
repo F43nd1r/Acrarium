57b8bcf8ea5650de90b12cf0624cb565eb5cea8c
f43nd1r <account+github@faendir.com>
2022-07-28 19:14:56 +0200

    more test fixes

--- acrarium/src/test/kotlin/com/faendir/acra/MariaDBTestConfiguration.kt
+++ acrarium/src/test/kotlin/com/faendir/acra/MariaDBTestConfiguration.kt
@@ -1,17 +1,25 @@
 package com.faendir.acra
 
 import ch.vorburger.mariadb4j.springframework.MariaDB4jSpringService
+import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @TestConfiguration
 class MariaDBTestConfiguration {
 
     @Bean
-    fun mariaDB4j(): MariaDB4jSpringService {
-        return MariaDB4jSpringService().apply {
-            configuration.addArg("--character-set-server=utf8mb4")
-            configuration.addArg("--collation-server=utf8mb4_unicode_ci")
-        }
+    fun mariaDB4j(): MariaDB4jSpringService = MariaDB4jSpringService().apply {
+        configuration.addArg("--character-set-server=utf8mb4")
+        configuration.addArg("--collation-server=utf8mb4_unicode_ci")
     }
+
+    @Bean
+    fun dataSource(mariaDB4j: MariaDB4jSpringService): DataSource = DataSourceBuilder.create()
+        .driverClassName("org.mariadb.jdbc.Driver")
+        .url("jdbc:mariadb://localhost:${mariaDB4j.configuration.port}/test")
+        .username("root")
+        .password(mariaDB4j.configuration.defaultRootPassword ?: "")
+        .build()
 }
--- acrarium/src/test/kotlin/com/faendir/acra/annotation/AcrariumTest.kt
+++ acrarium/src/test/kotlin/com/faendir/acra/annotation/AcrariumTest.kt
@@ -4,6 +4,7 @@ import ch.vorburger.mariadb4j.springboot.autoconfigure.MariaDB4jSpringConfigurat
 import com.faendir.acra.MariaDBTestConfiguration
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.context.annotation.Import
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -15,5 +16,8 @@ import java.lang.annotation.Inherited
 @ExtendWith(SpringExtension::class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @Import(MariaDBTestConfiguration::class)
-@EnableAutoConfiguration(exclude = [MariaDB4jSpringConfiguration::class])
+@EnableAutoConfiguration(
+    exclude = [MariaDB4jSpringConfiguration::class, DataSourceAutoConfiguration::class,
+        ch.vorburger.mariadb4j.springboot.autoconfigure.DataSourceAutoConfiguration::class]
+)
 annotation class AcrariumTest
