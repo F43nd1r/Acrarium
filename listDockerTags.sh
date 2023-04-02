a7be06e3625b1aeac7cad6ad15c161d483d0b4cc
f43nd1r <account+github@faendir.com>
2023-04-02 03:40:09 +0200

    get docker tags from bash because gradle spits out some unpreventable lines before.

--- listDockerTags.sh
+++ listDockerTags.sh
@@ -3,6 +3,10 @@
 IMAGE_NAME="f43nd1r/acrarium"
 VERSION="$(git describe --abbrev=0)"
 
+if [[ ${VERSION} == v* ]]; then
+  VERSION=${VERSION#v}
+fi
+
 function tag {
   echo "${IMAGE_NAME}:$1"
   echo "ghcr.io/${IMAGE_NAME}:$1"
